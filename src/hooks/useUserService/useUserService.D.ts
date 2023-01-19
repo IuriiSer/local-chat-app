74a96c4f8f07b6a23b9cc08de410d145b7243051
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 23:54:08 +0300

    add type union type UserServiceResponce

--- src/hooks/useUserService/useUserService.D.ts
+++ src/hooks/useUserService/useUserService.D.ts
@@ -1,10 +1,33 @@
-import { User } from '../../DataTypes/User/User.D';
+import { ServiceResponce } from '../../DataTypes/ServiceResponce.D';
+import { NewUser, User, UserFieldError } from '../../DataTypes/User/User.D';
 import { LoginI, LoginR } from './login.D';
 
-export type useUserService = {
-  user: User | null;
-	createNewUser: Function;
-	updateUserData: Function;
-	login: ({ login, password }: LoginI) => LoginR;
-	logout: Function;
+export type Logout = () => void;
+
+export enum UserServiceStatus {
+  ok,
+  error,
 }
+
+export enum UserServiceMessage {
+  ok = 'ok',
+  error = 'error',
+  wrongInput = 'wrongInput',
+  alreadyAuth = 'you are already authorized',
+}
+
+export type UserServiceResponce = ServiceResponce<
+  UserServiceStatus,
+  UserServiceMessage,
+  UserFieldError[] | null
+>;
+
+export type UseUserService = {
+  authorizedUser: User | null;
+  signin: (newUser: NewUser) => UserServiceResponce;
+  update: Function;
+  logout: Logout;
+  login: ({ login, password }: LoginI) => LoginR;
+};
+
+export type SetUserState = React.Dispatch<React.SetStateAction<User | null>>;
