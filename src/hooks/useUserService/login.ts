7a09a3f5855b7a146f50afa121c04a545c272725
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 13:12:21 +0300

    commit for fiture fix

--- src/hooks/useUserService/login.ts
+++ src/hooks/useUserService/login.ts
@@ -1,16 +1,13 @@
 import { LoginI, LoginR, LoginStatus } from './login.D';
-import { isUser } from '../../DataTypes/User/User.D';
-import getUsers from '../../lib/interfaces/users/getUsers';
 
 const login = ({ login, password }: LoginI): LoginR => {
 	const query = { userLogin: login };
-	const user = getUsers({ query });
+	// const user = getUsers({ query });
 
-	if (!user) return { status: LoginStatus.wrongLoginOrPassword };
-	if (!isUser(user, ['password'])) return { status: LoginStatus.wrongLoginOrPassword };
-	if (user.password !== password) return { status: LoginStatus.wrongLoginOrPassword };
-
-	return { status: LoginStatus.ok, user };
+	// if (!user) return { status: LoginStatus.wrongLoginOrPassword };
+	// if (!isUser(user, ['password'])) return { status: LoginStatus.wrongLoginOrPassword };
+	// if (user.password !== password) return { status: LoginStatus.wrongLoginOrPassword };
+	return { status: LoginStatus.ok };
 };
 
 export default login;
