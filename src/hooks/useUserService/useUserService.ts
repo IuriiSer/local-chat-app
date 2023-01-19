93b874228a2aed0daa07a18e1cbacdef841d247b
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 23:53:41 +0300

    add methods update, signin, login, logout

--- src/hooks/useUserService/useUserService.ts
+++ src/hooks/useUserService/useUserService.ts
@@ -1,12 +1,31 @@
-import React, { useState, useMemo } from 'react';
-import login from './login';
+import { useState } from 'react';
+import { NewUser, User } from '../../DataTypes/User/User.D';
 import Interfaces from '../../lib/interfaces';
+import loginPrototype from './login';
+import signinPrototype from './signin';
+import {
+  UserServiceMessage,
+  UserServiceResponce,
+  UserServiceStatus,
+  UseUserService,
+} from './useUserService.D';
 const { user: UserInterface } = Interfaces;
 
-const useUserService = () => {
-	const [user, setUser] = useState<null>(null);
+const useUserService = (): UseUserService => {
+  const [user, setUser] = useState<User | null>(null);
 
-	return { login, user };
+  const login = loginPrototype(UserInterface.getUsers, setUser);
+  const logout = () => setUser(null);
+
+  const tryToSignin = signinPrototype(UserInterface.addNewUser, setUser);
+  const signin = (newUser: NewUser): UserServiceResponce => {
+    if (!user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
+    return tryToSignin(newUser);
+  };
+
+  const update = () => {};
+
+  return { update, signin, login, logout, authorizedUser: user };
 };
 
 export default useUserService;
