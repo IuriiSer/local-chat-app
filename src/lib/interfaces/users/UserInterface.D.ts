5bcb01fc63e20290301003e3012fc7c612ed50df
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 23:52:50 +0300

    update types for methods

--- src/lib/interfaces/users/UserInterface.D.ts
+++ src/lib/interfaces/users/UserInterface.D.ts
@@ -1,13 +1,19 @@
 import { User, NewUser, UserFieldError } from '../../../DataTypes/User/User.D';
 import { GetUsersI, GetUsersR } from './lib/getUsers.D';
 
+export type GetUsers = ({ query }: GetUsersI) => GetUsersR;
+export type AddNewUser = ({ user }: { user: User }) => User;
+export type UpdateUserData = ({ user }: { user: User }) => User;
+	// some duplication but it necessary becuse a big semantic difference
+	// in another case the logic will be different
+
 // UserInterface Prototype
 // have 3 methods publick
 export abstract class UserInterfacePrototype {
-	abstract getUsers({ query }: GetUsersI): GetUsersR;
-		// get user/s by differnet query
-	abstract addNewUser({ newUser }: { newUser: NewUser }): User | UserFieldError[];
-		// add new user to the storage
-	abstract updateUserData({ user }: { user: User }): User | UserFieldError[];
-		// update the user by his id
+  abstract getUsers: GetUsers;
+  // get user/s by differnet query
+  abstract addNewUser: AddNewUser;
+  // add new user to the storage
+  abstract updateUserData: UpdateUserData;
+  // update the user by his id
 }
