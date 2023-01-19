03da12b11c4d1c1babedb2a8b8dcb908cc004cae
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 13:09:54 +0300

    add userInterface

--- src/hooks/useUserService/useUserService.ts
+++ src/hooks/useUserService/useUserService.ts
@@ -1,11 +1,12 @@
-import { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import login from './login';
+import Interfaces from '../../lib/interfaces';
+const { user: UserInterface } = Interfaces;
 
 const useUserService = () => {
 	const [user, setUser] = useState<null>(null);
 
-	return { login, user}
-
+	return { login, user };
 };
 
 export default useUserService;
