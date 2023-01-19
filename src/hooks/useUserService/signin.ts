a014477df45d97af4b1ce7471fc144fbadbd9bc3
IuriiSer <yusergeevi4@gmail.com>
2023-01-20 00:24:02 +0300

    rename validator method

--- src/hooks/useUserService/signin.ts
+++ src/hooks/useUserService/signin.ts
@@ -1,4 +1,4 @@
-import { isNewUser, NewUser, User } from '../../DataTypes/User/User.D';
+import { isTrueFormatUser, NewUser, User } from '../../DataTypes/User/User.D';
 import { AddNewUser } from '../../lib/interfaces/users/UserInterface.D';
 import { SetUserState, UserServiceMessage, UserServiceResponce, UserServiceStatus } from './useUserService.D';
 import { v4 as uuidv4 } from 'uuid';
@@ -10,7 +10,7 @@ const signinPrototype = (addUser: AddNewUser, setUser: SetUserState) => (newUser
     ...newUser,
   } as User;
 
-  const errs = isNewUser(user);
+  const errs = isTrueFormatUser(user);
   if (errs) return { status: UserServiceStatus.error, track: errs, message: UserServiceMessage.wrongInput };
 
   addUser({ user });
