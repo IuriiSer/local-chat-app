1f3bd4f7ab09d42f38c6b924fef2acedc9fd5831
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 13:09:31 +0300

    add user field validators + rebuild guards to new logic

--- src/DataTypes/User/User.D.ts
+++ src/DataTypes/User/User.D.ts
@@ -1,19 +1,20 @@
-import { version as uuidVersion, validate as uuidValidate } from 'uuid';
+import { version as uuidVersion, validate as uuidValidate, validate } from 'uuid';
 import { ChatID } from '../Chat/Chat.D';
+import { DataFormatError } from '../Errors.D';
 
 export type UserID = string;
-	// standart ID
+// standart ID
 export type UserLogin = string;
-	// the login to auth in user account
+// the login to auth in user account
 export type UserNickName = string;
-	// the nickname to show in chat and etc
+// the nickname to show in chat and etc
 export type UserPassword = string;
-	// user password to auth logining
+// user password to auth logining
 export type UserChat = {
 	_id: ChatID;
-		// chat id the user take apart
+	// chat id the user take apart
 	lastSeen: Date;
-		// lastseen time need to show a count of unreaded messages
+	// lastseen time need to show a count of unreaded messages
 };
 
 export type User = {
@@ -24,27 +25,24 @@ export type User = {
 	chats: UserChat[];
 };
 
-export type NewUser = Omit<User, '_id' | 'chat'>;
-	// data to create new user
-
-export type UserNewData = Partial<Omit<User, '_id' | 'chats'>> & Pick<User, '_id'>;
-	// data to update existed user
-	// like the NewUser but _id is required
+export type NewUser = Omit<User, '_id' | 'chats'>;
+// data to create new user
 
 export type UserOpenData = Omit<User, 'password' | 'chats' | 'session' | 'login'>;
-	// open user data that can be showed in front
+// open user data that can be showed in front
 
 export type UsersInStorage = { [key: UserID]: User };
-	// representaion of user in localStorage
-	// why object ->
-	// localStorage is slow and we will often send request to get UserOpenData
-	// it more faster to get data from {} by ID
-	// then use [].find() by id
+// representaion of user in localStorage
+// why object ->
+// localStorage is slow and we will often send request to get UserOpenData
+// it more faster to get data from {} by ID
+// then use [].find() by id
 
 /* TYPE GUARDS */
 // 'cause every user have access to storage, we shoud every time to check do our data from localStorage is
 // in requested format. We don`t need every time format in default server/client schema
 // but in this task it was necessary
+// THE MOST validators are simple and just check do toCheck contains requerid fields
 export const isUser = (toCheck: any, requiredFields: string[] | null): toCheck is User => {
 	if (typeof (toCheck as User) !== 'object') return false;
 	return !!!requiredFields?.some((field) => !toCheck[field]);
@@ -54,14 +52,101 @@ export const isUsersInStorage = (toCheck: any): toCheck is UsersInStorage => {
 	return typeof (toCheck as UsersInStorage) === 'object';
 };
 
-export const isUserNickName = (toCheck: any): toCheck is UserNickName => {
-	return typeof (toCheck as UserNickName) === 'string' && toCheck.length < 20;
+export const isUserNickName = (toCheck: string): toCheck is UserNickName => {
+	const nickNameValidator = getFieldValidator('isTrueNickName');
+	if (!nickNameValidator) return false;
+	return !!nickNameValidator.validate({ nickName: toCheck } as User);
 };
 
-export const isUserID = (toCheck: any): toCheck is UserID => {
-	return (
-		typeof (toCheck as UserNickName) === 'string' &&
-		uuidValidate(toCheck) &&
-		uuidVersion(toCheck) === 4
-	);
+export const isUserID = (toCheck: string): toCheck is UserID => {
+	const idValidator = getFieldValidator('isTrueID');
+	if (!idValidator) return false;
+	return !!idValidator.validate({ _id: toCheck } as User);
+};
+
+/* FIELDS ERRORS */
+// Errors codes
+export enum UserFieldsErrorDescription {
+	emptyId = 'Empty ID',
+	wrongId = 'Wrong ID format',
+	emptyLogin = 'Login is empty',
+	wrongLogin = 'Login have wrong format',
+	emptyNickName = 'NickName is empty',
+	wrongNickName = 'NickName have wrong format',
+	emptyPassword = 'Password is empty',
+	wrongPassword = 'Password have wrong format',
+}
+
+export type UserFieldError = DataFormatError<UserFieldsErrorDescription>;
+// format of field errors
+
+type UserFieldValidator = {
+	name: string;
+	// the unique name of validator
+	validate(user: User): UserFieldError | null;
+	// method that validate field
+};
+
+const userFieldValidators: UserFieldValidator[] = [
+	{
+		name: 'isTrueLogin',
+		validate(user: User): UserFieldError | null {
+			const { login } = user;
+			const field = 'login';
+			if (!login) return { field, err: UserFieldsErrorDescription.emptyLogin };
+			const regex = /^@([a-z0-9]+|(_|-[a-z0-9])+){1,}$/;
+			if (!regex.test(login)) return { field, err: UserFieldsErrorDescription.wrongLogin };
+			return null;
+		},
+	},
+	{
+		name: 'isTrueID',
+		validate(user: User): UserFieldError | null {
+			const { _id } = user;
+			const field = '_id';
+			if (!_id) return { field, err: UserFieldsErrorDescription.emptyId };
+			if (!uuidValidate(_id)) return { field, err: UserFieldsErrorDescription.wrongId };
+			if (uuidVersion(_id) !== 4) return { field, err: UserFieldsErrorDescription.wrongId };
+			return null;
+		},
+	},
+	{
+		name: 'isTrueNickName',
+		validate(user: User): UserFieldError | null {
+			const { nickName } = user;
+			const field = 'nickName';
+			if (!nickName) return { field, err: UserFieldsErrorDescription.emptyNickName };
+			const regex = /^([a-z0-9]+|(.[a-z0-9])+){1,}$/;
+			if (!regex.test(nickName)) return { field, err: UserFieldsErrorDescription.wrongNickName };
+			return null;
+		},
+	},
+	{
+		name: 'isTruePassword',
+		validate(user: User): UserFieldError | null {
+			const { password } = user;
+			const field = 'password';
+			if (!password) return { field, err: UserFieldsErrorDescription.emptyPassword };
+			if (password.length < 6) return { field, err: UserFieldsErrorDescription.wrongPassword };
+			const specSymbolRegex = /.{0,}\W/;
+			if (specSymbolRegex.test(password))
+				return { field, err: UserFieldsErrorDescription.wrongPassword };
+			return null;
+		},
+	},
+];
+
+const getFieldValidator = (name: string): UserFieldValidator | null => {
+	return userFieldValidators.find((validator) => validator.name === name) || null;
+};
+
+export const isNewUser = (newUser: User): UserFieldError[] | null => {
+	const dataErrors = [] as UserFieldError[];
+	userFieldValidators.forEach(({ validate }) => {
+		const err = validate(newUser);
+		if (!err) return;
+		dataErrors.push(err);
+	});
+	if (dataErrors.length) return dataErrors;
+	return null;
 };
