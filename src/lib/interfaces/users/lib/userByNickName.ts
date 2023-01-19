f2222fa2ffd5af97ea240fe69bdc5b16ce66531f
IuriiSer <yusergeevi4@gmail.com>
2023-01-19 13:12:02 +0300

    fix imports

--- src/lib/interfaces/users/lib/userByNickName.ts
+++ src/lib/interfaces/users/lib/userByNickName.ts
@@ -1,5 +1,5 @@
-import { UserByNickNameI, UserByNickNameR } from '../../../DataTypes/App/lib/interfaces/users/UsersByNickName';
-import { isUser } from '../../../DataTypes/Data/User/User';
+import { isUser } from '../../../../DataTypes/User/User.D';
+import { UserByNickNameI, UserByNickNameR } from './UsersByNickName.D';
 
 const userByNickName = ({ users, userNickName }: UserByNickNameI): UserByNickNameR => {
 	return users.find((user) => isUser(user, ['nickName']) && user.nickName === userNickName) || null;
