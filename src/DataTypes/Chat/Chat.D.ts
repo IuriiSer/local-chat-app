7829036412e3263067759e4646af82ed40a7d36b
IuriiSer <yusergeevi4@gmail.com>
2023-01-22 00:03:35 +0300

    update Chat type

--- src/DataTypes/Chat/Chat.D.ts
+++ src/DataTypes/Chat/Chat.D.ts
@@ -4,14 +4,18 @@ import { MessageID } from '../Message/Message.D';
 export type ChatID = string;
 
 export type Chat = {
-	_id: ChatID;
-		// Hold chat ID
-	users: UserID[];
-		// Hold all users IDs that joined to the chat
-	messages: MessageID[];
-		// Hold all messages IDs that sent in the chat
-	owner: UserID;
-		// The owner of chat
-	managers: UserID[];
-		// Users who can approve new applications to join the chat
+  _id: ChatID;
+  // Hold chat ID
+  users: UserID[];
+  // Hold all users IDs that joined to the chat
+  messages: MessageID[];
+  // Hold all messages IDs that sent in the chat
+  owner: UserID;
+  // The owner of chat
+  managers: UserID[];
+  // Users who can approve new applications to join the chat
+};
+
+export type ChatsCache = {
+  [key: ChatID]: Chat;
 };
