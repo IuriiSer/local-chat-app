a1b396e3242cb1c2406205c632e4270be1b1933b
IuriiSer <yusergeevi4@gmail.com>
2023-01-22 00:07:32 +0300

    update logic for methods + add extra validators

--- src/hooks/useUserService/useUserService.D.ts
+++ src/hooks/useUserService/useUserService.D.ts
@@ -1,12 +1,19 @@
 import { ServiceResponce } from '../../DataTypes/ServiceResponce.D';
-import { NewUser, User, UserFieldError } from '../../DataTypes/User/User.D';
-import { LoginI, LoginR } from './login.D';
+import {
+  NewUser,
+  User,
+  UserDataToUpdate,
+  UserFieldError,
+  UserOpenData,
+} from '../../DataTypes/User/User.D';
+import { GetUsersI } from '../../lib/interfaces/users/lib/getUsers.D';
+import { LoginI, LoginR } from './signinPrototype.D';
 
 export type Logout = () => void;
 
 export enum UserServiceStatus {
-  ok,
-  error,
+  ok = 'ok',
+  error = 'error',
 }
 
 export enum UserServiceMessage {
@@ -23,12 +30,16 @@ export type UserServiceResponce = ServiceResponce<
   UserFieldError[] | null
 >;
 
+export type Signup = (newUser: NewUser) => UserServiceResponce;
+export type Signin = ({ login, password }: LoginI) => UserServiceResponce;
+
 export type UseUserService = {
   authorizedUser: User | null;
-  signin: (newUser: NewUser) => UserServiceResponce;
-  update: Function;
-  logout: Logout;
-  login: ({ login, password }: LoginI) => LoginR;
+  signup: Signup;
+  update: (newData: UserDataToUpdate) => UserServiceResponce;
+  logout: () => void;
+  signin: Signin;
+  getUsers: ({ query }: GetUsersI) => UserOpenData | UserOpenData[] | null;
 };
 
 export type SetUserState = React.Dispatch<React.SetStateAction<User | null>>;
--- src/hooks/useUserService/useUserService.ts
+++ src/hooks/useUserService/useUserService.ts
@@ -1,8 +1,17 @@
 import { useState } from 'react';
-import { NewUser, User, UserDataToUpdate } from '../../DataTypes/User/User.D';
+import {
+  NewUser,
+  User,
+  UserDataToUpdate,
+  UserFieldsErrorDescription,
+  UserOpenData,
+} from '../../DataTypes/User/User.D';
+import convertOpenUserData from '../../lib/converters/users/convertOpenUserData';
 import Interfaces from '../../lib/interfaces';
-import loginPrototype from './login';
-import signinPrototype from './signin';
+import { GetUsersI } from '../../lib/interfaces/users/lib/getUsers.D';
+import signinPrototype from './signinPrototype';
+import { LoginI } from './signinPrototype.D';
+import signupPrototype from './signupPrototype';
 import updatePrototype from './update';
 import {
   UserServiceMessage,
@@ -15,22 +24,60 @@ const { user: UserInterface } = Interfaces;
 const useUserService = (): UseUserService => {
   const [user, setUser] = useState<User | null>(null);
 
-  const login = loginPrototype(UserInterface.getUsers, setUser);
-  const logout = () => setUser(null);
-
-  const tryToSignin = signinPrototype(UserInterface.addNewUser, setUser);
-  const signin = (newUser: NewUser): UserServiceResponce => {
-    if (!user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
-    return tryToSignin(newUser);
+  /* SIGIN logic */
+  const tryToSignin = signinPrototype(UserInterface.getUsers, setUser);
+  const signin = ({ login, password }: LoginI): UserServiceResponce => {
+    if (user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
+    return tryToSignin({ login, password });
   };
+  /* END */
 
-	const tryToUpdate= updatePrototype(UserInterface.updateUserData, setUser)
-	const update = (newData: UserDataToUpdate) => {
+  /* LOGOUT logic */
+  const logout = () => {
+    UserInterface.eraseCache();
+    setUser(null);
+  };
+  /* END */
+
+  /* SIGNUP / REGISTRATION logic */
+  const tryToSignup = signupPrototype(UserInterface.addNewUser, setUser);
+  const signup = (newUser: NewUser): UserServiceResponce => {
+    if (user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
+    if (newUser.password !== newUser.passwordRepeat)
+      return {
+        status: UserServiceStatus.error,
+        track: [
+          { field: 'password', err: UserFieldsErrorDescription.passwordIsNotSame },
+          { field: 'passwordRepeat', err: UserFieldsErrorDescription.passwordIsNotSame },
+        ],
+      };
+    if (getUsers({ query: { userLogin: newUser.login } }))
+      return {
+        status: UserServiceStatus.error,
+        track: [{ field: 'nickName', err: UserFieldsErrorDescription.loginIsBusy }],
+      };
+    return tryToSignup(newUser);
+  };
+  /* END */
+
+  /* UPDATE user info logic */
+  const tryToUpdate = updatePrototype(UserInterface.updateUserData, setUser);
+  const update = (newData: UserDataToUpdate): UserServiceResponce => {
     if (!user) return { status: UserServiceStatus.error, message: UserServiceMessage.notAuth };
     return tryToUpdate(user, newData);
   };
+  /* END */
 
-  return { update, signin, login, logout, authorizedUser: user };
+  /* GET USERS open info logic */
+  const getUsers = ({ query }: GetUsersI): UserOpenData | UserOpenData[] | null => {
+    const users = UserInterface.getUsers({ query });
+    if (!users) return null;
+    if (Array.isArray(users)) return users.map(convertOpenUserData);
+    return convertOpenUserData(users);
+  };
+  /* END */
+
+  return { update, signup, signin, logout, authorizedUser: user, getUsers };
 };
 
 export default useUserService;
