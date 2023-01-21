721fd52f695353fa411ac5708f3ded59a3bbf564
IuriiSer <yusergeevi4@gmail.com>
2023-01-22 00:07:52 +0300

    tab fixes

--- src/lib/drivers/storage/storageDriver.ts
+++ src/lib/drivers/storage/storageDriver.ts
@@ -3,48 +3,49 @@ import convertToLocalStorage from '../../converters/localStorage/convertToLocalS
 import convertFromLocalStorage from '../../converters/localStorage/convertFromLocalStorage';
 
 const storageDriver = <T extends Object | []>({ fieldName }: StorageDriverI): StorageDriverR<T> => {
-	const writeDataInStorage = ({ newData }: DataInStorageI<T>): void => {
-		localStorage.setItem(fieldName, convertToLocalStorage(newData));
-	};
+  const writeDataInStorage = ({ newData }: DataInStorageI<T>): void => {
+    localStorage.setItem(fieldName, convertToLocalStorage(newData));
+  };
 
-	const eraseDataInStorage = (): void => {
-		localStorage.removeItem(fieldName);
-	};
+  const eraseDataInStorage = (): void => {
+    localStorage.removeItem(fieldName);
+  };
 
-	const addDataInStorage = ({ newData }: DataInStorageI<T>): void => {
-		const rawData = localStorage.getItem(fieldName);
-		let data = null;
-		if (rawData) data = convertFromLocalStorage<T>(rawData);
+  const addDataInStorage = ({ newData }: DataInStorageI<T>): void => {
+    const rawData = localStorage.getItem(fieldName);
+    let data = null;
 
-		if (!data) {
-			writeDataInStorage({ newData });
-			return;
-		}
+    if (rawData) data = convertFromLocalStorage<T>(rawData);
 
-		if (typeof data !== typeof newData)
-			throw new TypeError('DataTypes is not match. Try to write\n', { cause: newData });
+    if (!data) {
+      writeDataInStorage({ newData });
+      return;
+    }
 
-		if (Array.isArray(newData)) {
-			if (!data) data = [] as T;
-			if (Array.isArray(data)) writeDataInStorage({ newData: [...data, ...newData] as T });
-			return;
-		}
+    if (typeof data !== typeof newData)
+      throw new TypeError('DataTypes is not match. Try to write\n', { cause: newData });
 
-		if (typeof newData === 'object') {
-			if (!data) data = {} as T;
-			Object.assign(data, newData);
-			writeDataInStorage({ newData: data as T });
-			return;
-		}
-	};
+    if (Array.isArray(newData)) {
+      if (!data) data = [] as T;
+      if (Array.isArray(data)) writeDataInStorage({ newData: [...data, ...newData] as T });
+      return;
+    }
 
-	const getDataInStorage = (): T | null => {
-		const rawData = localStorage.getItem(fieldName);
-		if (!rawData) return null;
-		return convertFromLocalStorage<T>(rawData);
-	};
+    if (typeof newData === 'object') {
+      if (!data) data = {} as T;
+      Object.assign(data, newData);
+      writeDataInStorage({ newData: data as T });
+      return;
+    }
+  };
 
-	return { writeDataInStorage, eraseDataInStorage, getDataInStorage, addDataInStorage };
+  const getDataInStorage = (): T | null => {
+    const rawData = localStorage.getItem(fieldName);
+    if (!rawData) return null;
+    return convertFromLocalStorage<T>(rawData);
+  };
+
+  return { writeDataInStorage, eraseDataInStorage, getDataInStorage, addDataInStorage };
 };
 
 export default storageDriver;
