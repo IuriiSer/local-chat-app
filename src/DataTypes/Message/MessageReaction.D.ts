7f04a74e9ac2f33006a80ae8535ab39b373a0349
IuriiSer <yusergeevi4@gmail.com>
2023-01-23 11:21:27 +0300

    add type for new Reaction

--- src/DataTypes/Message/MessageReaction.D.ts
+++ src/DataTypes/Message/MessageReaction.D.ts
@@ -3,8 +3,15 @@ import { UserID } from '../User/User.D';
 type EmojiID = string;
 
 export type MessageReaction = {
-	emoji: EmojiID;
-		// the emoji ID
-	users: UserID[];
-		// users IDs who leavs the reaction
+  emoji: EmojiID;
+  // the emoji ID
+  users: UserID[];
+  // users IDs who leavs the reaction
+};
+
+export type MessageNewReaction = {
+  emoji: EmojiID;
+  // the emoji ID
+  userID: UserID;
+  // the user who leave the Reaction
 };
