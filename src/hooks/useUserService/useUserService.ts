d7c4889c65b1abcf2ca30333960d5cc5dd5d158f
IuriiSer <yusergeevi4@gmail.com>
2023-01-23 11:24:41 +0300

    add callbacks optimisation

--- src/hooks/useUserService/useUserService.ts
+++ src/hooks/useUserService/useUserService.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import {
   NewUser,
   User,
@@ -25,11 +25,14 @@ const useUserService = (): UseUserService => {
   const [user, setUser] = useState<User | null>(null);
 
   /* SIGIN logic */
-  const tryToSignin = signinPrototype(UserInterface.getUsers, setUser);
-  const signin = ({ login, password }: LoginI): UserServiceResponce => {
-    if (user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
-    return tryToSignin({ login, password });
-  };
+  const signin = useCallback(
+    ({ login, password }: LoginI): UserServiceResponce => {
+      const tryToSignin = signinPrototype(UserInterface.getByQuery, setUser);
+      if (user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
+      return tryToSignin({ login, password });
+    },
+    [user],
+  );
   /* END */
 
   /* LOGOUT logic */
@@ -39,42 +42,48 @@ const useUserService = (): UseUserService => {
   };
   /* END */
 
-  /* SIGNUP / REGISTRATION logic */
-  const tryToSignup = signupPrototype(UserInterface.addNewUser, setUser);
-  const signup = (newUser: NewUser): UserServiceResponce => {
-    if (user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
-    if (newUser.password !== newUser.passwordRepeat)
-      return {
-        status: UserServiceStatus.error,
-        track: [
-          { field: 'password', err: UserFieldsErrorDescription.passwordIsNotSame },
-          { field: 'passwordRepeat', err: UserFieldsErrorDescription.passwordIsNotSame },
-        ],
-      };
-    if (getUsers({ query: { userLogin: newUser.login } }))
-      return {
-        status: UserServiceStatus.error,
-        track: [{ field: 'nickName', err: UserFieldsErrorDescription.loginIsBusy }],
-      };
-    return tryToSignup(newUser);
-  };
-  /* END */
-
-  /* UPDATE user info logic */
-  const tryToUpdate = updatePrototype(UserInterface.updateUserData, setUser);
-  const update = (newData: UserDataToUpdate): UserServiceResponce => {
-    if (!user) return { status: UserServiceStatus.error, message: UserServiceMessage.notAuth };
-    return tryToUpdate(user, newData);
-  };
-  /* END */
-
   /* GET USERS open info logic */
-  const getUsers = ({ query }: GetUsersI): UserOpenData | UserOpenData[] | null => {
-    const users = UserInterface.getUsers({ query });
+  const getUsers = useCallback(({ query }: GetUsersI): UserOpenData | UserOpenData[] | null => {
+    const users = UserInterface.getByQuery({ query });
     if (!users) return null;
     if (Array.isArray(users)) return users.map(convertOpenUserData);
     return convertOpenUserData(users);
-  };
+  }, []);
+  /* END */
+
+  /* SIGNUP / REGISTRATION logic */
+  const signup = useCallback(
+    (newUser: NewUser): UserServiceResponce => {
+      const tryToSignup = signupPrototype(UserInterface.addNew, setUser);
+      if (user) return { status: UserServiceStatus.error, message: UserServiceMessage.alreadyAuth };
+      if (newUser.password !== newUser.passwordRepeat)
+        return {
+          status: UserServiceStatus.error,
+          track: [
+            { field: 'password', err: UserFieldsErrorDescription.passwordIsNotSame },
+            { field: 'passwordRepeat', err: UserFieldsErrorDescription.passwordIsNotSame },
+          ],
+        };
+      if (getUsers({ query: { userLogin: newUser.login } }))
+        return {
+          status: UserServiceStatus.error,
+          track: [{ field: 'nickName', err: UserFieldsErrorDescription.loginIsBusy }],
+        };
+      return tryToSignup(newUser);
+    },
+    [user, getUsers],
+  );
+  /* END */
+
+  /* UPDATE user info logic */
+  const update = useCallback(
+    (newData: UserDataToUpdate): UserServiceResponce => {
+      const tryToUpdate = updatePrototype(UserInterface.updateData, setUser);
+      if (!user) return { status: UserServiceStatus.error, message: UserServiceMessage.notAuth };
+      return tryToUpdate(user, newData);
+    },
+    [user],
+  );
   /* END */
 
   return { update, signup, signin, logout, authorizedUser: user, getUsers };
