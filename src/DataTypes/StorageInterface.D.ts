72abe28ee73a60188dafe31f02c91d2de853ea62
IuriiSer <yusergeevi4@gmail.com>
2023-01-23 11:19:50 +0300

    common storage interface factory

---
+++ src/DataTypes/StorageInterface.D.ts
@@ -0,0 +1,12 @@
+// Common interface tamlate for all Interfaces that work with localStorage
+export abstract class StorageInterface<G, U, A> {
+  abstract getByQuery: G;
+  // get all entyties ()
+  abstract addNew: U;
+  // add new user to the storage
+  abstract updateData: A;
+  // update the user by his id
+
+  abstract eraseCache: () => void;
+  // method to erase cache
+}
