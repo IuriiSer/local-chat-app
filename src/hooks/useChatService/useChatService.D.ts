0d52db3495b7580c4d970b3217558e0f975958f0
IuriiSer <yusergeevi4@gmail.com>
2023-01-23 11:22:48 +0300

    add new logic to work with chats + rebuild some code

--- src/hooks/useChatService/useChatService.D.ts
+++ src/hooks/useChatService/useChatService.D.ts
@@ -1,6 +1,20 @@
+import { Chat, ChatID } from '../../DataTypes/Chat/Chat.D';
+import { UserID } from '../../DataTypes/User/User.D';
+import { Action, ActionType } from '../../lib/interfaces/broadcastChannel/BroadcastInterface.D';
+
 export type useMessageService = {};
 
 export enum ChatServiceStatus {
   online = 'online',
   offline = 'offline',
 }
+
+export type UseChatService = {
+  activeChatID: ChatID | null;
+  chatServiceStatus: ChatServiceStatus;
+  setActiveChatID: (chatID: ChatID) => void;
+  getChats: () => Chat[] | null;
+  useSubscribe: (action: Action, actionType: ActionType) => void;
+  createNewChat: (toUserID: UserID) => void;
+  exitFromChat: (chatID: ChatID) => void;
+};
--- src/hooks/useChatService/useChatService.ts
+++ src/hooks/useChatService/useChatService.ts
@@ -1,29 +1,32 @@
-import { useRef, useEffect, useState } from 'react';
-import { ChatID } from '../../DataTypes/Chat/Chat.D';
-import { User } from '../../DataTypes/User/User.D';
+import { useRef, useEffect, useState, useCallback } from 'react';
+import { Chat, ChatID } from '../../DataTypes/Chat/Chat.D';
+import { User, UserID } from '../../DataTypes/User/User.D';
 import Interfaces from '../../lib/interfaces';
-import { BroadcastInterface } from '../../lib/interfaces/broadcastChannel/BroadcastInterface.D';
-import { ChatServiceStatus } from './useChatService.D';
+import {
+  Action,
+  ActionType,
+  BroadcastEents,
+  BroadcastInterface,
+} from '../../lib/interfaces/broadcastChannel/BroadcastInterface.D';
+import { ChatServiceStatus, UseChatService } from './useChatService.D';
+import { v4 as uuidv4 } from 'uuid';
+import { MessageID } from '../../DataTypes/Message/Message.D';
 
 const Channel = 'local-chat';
 
-const useChatService = (user: User | null) => {
+const useChatService = (user: User | null): UseChatService => {
   const [activeChatID, _setActiveChatID] = useState<ChatID | null>(null);
   const [chatServiceStatus, setChatServiceStatus] = useState<ChatServiceStatus>(
     ChatServiceStatus.offline,
   );
-  const channel = useRef<BroadcastInterface | null>(null);
-
-  const setActiveChatID = (chatID: ChatID): void => {
-    if (chatID && chatID === activeChatID) return;
-    _setActiveChatID(chatID);
-  };
-
+  /**
+   * Logic to open new bcInterface when user will authorize
+   */
+  const bcInterface = useRef<BroadcastInterface | null>(null);
   useEffect(() => {
     if (!user) {
-      if (channel.current) {
-        channel.current.close();
-        channel.current = null;
+      if (bcInterface.current) {
+        bcInterface.current.close();
         setChatServiceStatus(ChatServiceStatus.offline);
       }
       return;
@@ -31,22 +34,108 @@ const useChatService = (user: User | null) => {
     console.group();
     console.log('open broadcastChanel');
     console.log('for user');
-    console.log(user)
+    console.log(user);
     console.groupEnd();
-    
+
     setChatServiceStatus(ChatServiceStatus.online);
-    channel.current = Interfaces.broadcastPrototype(Channel, user.chats);
-    
+    bcInterface.current = Interfaces.broadcastPrototype(Channel);
+
     return () => {
-      if (channel.current) {
-        channel.current.close();
-        channel.current = null;
+      if (bcInterface.current) {
+        bcInterface.current.close();
         setChatServiceStatus(ChatServiceStatus.offline);
       }
     };
   }, [user]);
 
-  return { setActiveChatID, activeChatID, chatServiceStatus };
+  /**
+   * Set ACtive chat handler
+   */
+  const setActiveChatID = useCallback(
+    (chatID: ChatID): void => {
+      if (chatID && chatID === activeChatID) return;
+      _setActiveChatID(chatID);
+    },
+    [activeChatID],
+  );
+
+  /**
+   * Logic to get all User chats data
+   */
+  const getChats = useCallback((): Chat[] | null => {
+    if (!user) return null;
+    return Interfaces.chat.getByQuery({ chatIDs: user.chats.map((chat) => chat._id) });
+  }, [user]);
+
+  /**
+   * Hook to subscribe to Broadcast.omMessage events
+   * @param action Action that will fire on actionType event
+   * @param actionType ActionType
+   */
+  const useSubscribe = (action: Action, actionType: ActionType) => {
+    useEffect(() => {
+      if (!bcInterface.current) return;
+      const _id = uuidv4();
+      bcInterface.current.subscribe({ _id, actionType, action });
+
+      console.group();
+      console.log('New subscriber');
+      console.log(`For ${actionType} messages`);
+      console.log(action);
+      console.groupEnd();
+
+      return () => {
+        if (!bcInterface.current) return;
+        bcInterface.current.unsubscribe({ idToUnsubscribe: _id, actionType });
+      };
+    }, []);
+  };
+
+  /**
+   * Logic to create new chat and fire event in bcInterface
+   */
+  const createNewChat = useCallback(
+    (toUserID: UserID) => {
+      if (!user || !bcInterface.current) return;
+      const newChat = {
+        _id: uuidv4(),
+        users: [user._id, toUserID],
+        messages: [] as MessageID[],
+        owner: user._id,
+      };
+      Interfaces.chat.addNew(newChat);
+      bcInterface.current.sentData({
+        event: BroadcastEents.userHasInvitedToChat,
+        content: { chatID: newChat._id, from: user._id, to: toUserID },
+      });
+    },
+    [user],
+  );
+
+  const exitFromChat = useCallback(
+    (chatID: ChatID) => {
+      if (!user) return;
+      const res = Interfaces.chat.getByQuery({ chatIDs: [chatID] });
+      if (!res) return;
+      const [chatData] = res;
+
+      const newChatUsers = chatData.users.filter((userInChat) => userInChat !== user._id);
+      if (newChatUsers.length === chatData.users.length) return;
+
+      Interfaces.chat.updateData({ ...chatData, users: newChatUsers });
+    },
+    [user],
+  );
+
+  return {
+    getChats,
+    setActiveChatID,
+    activeChatID,
+    chatServiceStatus,
+    useSubscribe,
+    createNewChat,
+    exitFromChat,
+  };
 };
 
 export default useChatService;
