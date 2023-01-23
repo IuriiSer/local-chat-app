711ab69a1c6ee941af5b16e43ffdd0145229c88d
IuriiSer <yusergeevi4@gmail.com>
2023-01-23 11:24:06 +0300

    add input validators

--- src/hooks/useUserService/signinPrototype.ts
+++ src/hooks/useUserService/signinPrototype.ts
@@ -1,7 +1,17 @@
-import { isUser, User } from '../../DataTypes/User/User.D';
+import {
+  getFieldValidators,
+  isCustomTrue,
+  isUser,
+  User,
+  UserFieldsErrorDescription,
+} from '../../DataTypes/User/User.D';
 import { GetUsers } from '../../lib/interfaces/users/UserInterface.D';
-import { LoginI, LoginR, LoginStatus } from './signinPrototype.D';
-import { UserServiceMessage, UserServiceResponce, UserServiceStatus } from './useUserService.D';
+import { LoginI, LoginStatus } from './signinPrototype.D';
+import { UserServiceResponce, UserServiceStatus } from './useUserService.D';
+
+const trueLoginValidators = getFieldValidators(['isTrueLogin', 'isTruePassword']);
+if (!trueLoginValidators)
+  throw new Error('Empty valodator. src/hooks/useUserService/signinPrototype.ts');
 
 const signinPrototype =
   (getUsers: GetUsers, setUser: React.Dispatch<React.SetStateAction<User | null>>) =>
@@ -9,12 +19,33 @@ const signinPrototype =
     const query = { userLogin: login };
     const user = getUsers({ query }) as User | null;
 
+    const inputErrors = isCustomTrue({ login, password } as User, trueLoginValidators);
+    if (inputErrors)
+      return {
+        status: UserServiceStatus.error,
+        track: inputErrors,
+      };
     if (!user)
-      return { status: UserServiceStatus.error, message: LoginStatus.wrongLoginOrPassword };
-    if (!isUser(user, ['password']))
-      return { status: UserServiceStatus.error, message: LoginStatus.errorInStorage };
+      return {
+        status: UserServiceStatus.error,
+        track: [
+          {
+            field: 'login',
+            err: UserFieldsErrorDescription.wrongSigninLogin,
+          },
+        ],
+      };
+    if (!isUser(user, ['password'])) throw new Error(LoginStatus.errorInStorage);
     if (user.password !== password)
-      return { status: UserServiceStatus.error, message: LoginStatus.wrongLoginOrPassword };
+      return {
+        status: UserServiceStatus.error,
+        track: [
+          {
+            field: 'password',
+            err: UserFieldsErrorDescription.wrongSigninPassword,
+          },
+        ],
+      };
     setUser(user);
     return { status: UserServiceStatus.ok };
   };
