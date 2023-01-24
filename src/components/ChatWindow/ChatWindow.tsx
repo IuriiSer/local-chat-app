d110124f346ec50bd01cf1022d868760e6dfee46
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:40:41 +0300

    main chat window

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,20 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect, ChangeEvent, useCallback } from 'react';
 import Grid from '@mui/material/Grid';
-import { useTheme } from '@mui/material';
+import { Divider, Typography, useTheme } from '@mui/material';
 import AppContext from '../AppContext/AppContext';
-import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import Typography from '@mui/material/Typography';
-import FormControl from '@mui/material/FormControl';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import TextAreaWithAdorment from '../TextAreaWithAdorment/TextAreaWithAdorment';
+import NearMeIcon from '@mui/icons-material/NearMe';
+import { Message } from '../../DataTypes/Message/Message.D';
+import MessageDrawer from '../MessageDrawer/MessageDrawer';
+import { SetMessage } from '../../hooks/useActiveChatManager';
 
-const ChatWindow = () => {
+interface ChatWindowI {
+  messages: Message[];
+  setMessages: SetMessage;
+}
+
+const ChatWindow = ({ messages, setMessages }: ChatWindowI) => {
   const theme = useTheme();
   const {
+    messageService: { addMessage: sentMessage },
     chatService: { activeChatID },
   } = useContext(AppContext);
+
+  const [typedText, setTypedText] = useState<string>('');
+
+  const sentMessageHandler = useCallback(() => {
+    if (!activeChatID) return;
+    if (!typedText.trim()) return;
+    const newMessage = sentMessage(activeChatID, typedText.trim());
+    if (!newMessage) return;
+    setMessages((prev) => [...prev, newMessage]);
+    setTypedText('');
+  }, [sentMessage, activeChatID, typedText, setMessages]);
+
+  useEffect(() => {
+    const element = document.getElementById(messages.at(-1)?._id || '');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
 
   if (!activeChatID)
     return (
@@ -27,25 +50,48 @@ const ChatWindow = () => {
         </Typography>
       </Grid>
     );
+
   return (
-    <Grid container direction='column-reverse' sx={{ padding: theme.spacing(1), flexGrow: 1 }}>
+    <Grid
+      container
+      direction='column-reverse'
+      sx={{
+        padding: theme.spacing(1),
+        flexGrow: 1,
+        gap: theme.spacing(1),
+        height: '100%',
+        overflow: 'hidden',
+        flexWrap: 'nowrap',
+      }}>
       {/* SNET Button */}
       <Grid item>
-        <FormControl sx={{ width: '100%' }} variant='outlined'>
-          <InputLabel>Type a message</InputLabel>
-          <OutlinedInput
-            id='message-input'
-            type='text'
-            endAdornment={
-              <InputAdornment position='end'>
-                <IconButton aria-label='toggle password visibility' onClick={() => {}} edge='end'>
-                  <InsertEmoticonIcon />
-                </IconButton>
-              </InputAdornment>
-            }
-            label='messageInput'
-          />
-        </FormControl>
+        <TextAreaWithAdorment
+          label='Type a message'
+          fieldKey='message'
+          value={typedText}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setTypedText(e.target.value);
+          }}
+          actionCallBacks={[sentMessageHandler, () => {}]}>
+          <NearMeIcon />
+          <InsertEmoticonIcon />
+        </TextAreaWithAdorment>
+      </Grid>
+      <Divider />
+      <Grid
+        container
+        direction='column'
+        sx={{
+          paddingBottom: theme.spacing(1),
+          gap: theme.spacing(1),
+          overflowY: 'auto',
+          flexWrap: 'nowrap',
+        }}>
+        {messages.map((message) => (
+          <Grid item key={message._id}>
+            <MessageDrawer {...message} />
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
