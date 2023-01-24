02968e55b25ab8c3c0c74c1d8731da3840b27a04
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:38:28 +0300

    add new fields to context tipisation

--- src/components/AppContext/AppContext.D.ts
+++ src/components/AppContext/AppContext.D.ts
@@ -1,7 +1,11 @@
+import { UseBroadcastService } from "../../hooks/useBroadcastService/useBroadcastService.D";
 import { UseChatService } from "../../hooks/useChatService/useChatService.D";
+import { UseMessageService } from "../../hooks/useMessageService/useMessageService.D";
 import { UseUserService } from "../../hooks/useUserService/useUserService.D";
 
 export type AppContextT = {
   userService: UseUserService
   chatService: UseChatService
+  bcService: UseBroadcastService
+  messageService: UseMessageService
 }
