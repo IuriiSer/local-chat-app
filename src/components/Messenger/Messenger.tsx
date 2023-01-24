5b8fefa3c872f858618c01eaff4d0948127b23cc
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:41:28 +0300

    split main logic to sub components

--- src/components/Messenger/Messenger.tsx
+++ src/components/Messenger/Messenger.tsx
@@ -1,48 +1,25 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import MessengerHeader from '../MessengerHeader/MessengerHeader';
-import SideBarMenu from '../SideBarMenu/SideBarMenu';
-import ChatWindow from '../ChatWindow/ChatWindow';
+import { UserChatExtended } from '../../DataTypes/User/User.D';
+import useActiveChatManager from '../../hooks/useActiveChatManager';
+import useChatsManager from '../../hooks/useChatsManager';
+import MessengerUI from './MessengerUI';
 
-const Main = styled('main')(({ theme }) => ({
-  padding: theme.spacing(0, 3, 2),
-  width: '100%',
-  height: '100%',
-  position: 'absolute',
-  display: 'flex',
-  flexDirection: 'column',
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
-
-export default function Messenger() {
-  const [open, setOpen] = React.useState(true);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+const Messanger = () => {
+  const { chatsData } = useChatsManager();
   return (
-    <Box sx={{ display: 'flex' }}>
-      <MessengerHeader open={open} handleDrawerOpen={handleDrawerOpen} />
-      <SideBarMenu open={open} handleDrawerClose={handleDrawerClose} />
-      <Main>
-        <DrawerHeader />
-        <ChatWindow />
-      </Main>
-    </Box>
+    <>
+      <AddActiveChatManager chatsData={chatsData} />
+    </>
   );
-}
+};
+
+const AddActiveChatManager = ({ chatsData }: { chatsData: UserChatExtended[] }) => {
+  const { messages, setMessages } = useActiveChatManager();
+  return (
+    <>
+      <MessengerUI chatsData={chatsData} messages={messages} setMessages={setMessages} />
+    </>
+  );
+};
+
+export default Messanger;
---
+++ src/components/Messenger/MessengerUI.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MessengerHeader from '../MessengerHeader/MessengerHeader';
+import ChatSideBarManager from '../ChatSideBarManager/ChatSideBarManager';
+import ChatWindow from '../ChatWindow/ChatWindow';
+import { UserChatExtended } from '../../DataTypes/User/User.D';
+import { Message } from '../../DataTypes/Message/Message.D';
+import { SetMessage } from '../../hooks/useActiveChatManager';
+
+const Main = styled('main')(({ theme }) => ({
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+interface MessengerUII {
+  chatsData: UserChatExtended[];
+  messages: Message[];
+  setMessages: SetMessage;
+}
+
+export default function MessengerUI({ chatsData, messages, setMessages }: MessengerUII) {
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <MessengerHeader open={open} handleDrawerOpen={handleDrawerOpen} />
+      <ChatSideBarManager chatsData={chatsData} open={open} handleDrawerClose={handleDrawerClose} />
+      <Main>
+        <DrawerHeader />
+        <ChatWindow messages={messages} setMessages={setMessages} />
+      </Main>
+    </Box>
+  );
+}
