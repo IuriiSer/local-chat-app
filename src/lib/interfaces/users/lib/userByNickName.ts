121bcf021815a7c91a30cc0de377df266a87bca2
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:50:18 +0300

    add search user by nickname regexp

--- src/lib/interfaces/users/lib/userByNickName.ts
+++ src/lib/interfaces/users/lib/userByNickName.ts
@@ -2,7 +2,8 @@ import { isUser } from '../../../../DataTypes/User/User.D';
 import { UserByNickNameI, UserByNickNameR } from './UsersByNickName.D';
 
 const userByNickName = ({ users, userNickName }: UserByNickNameI): UserByNickNameR => {
-	return users.find((user) => isUser(user, ['nickName']) && user.nickName === userNickName) || null;
+  const regexp = new RegExp(userNickName);
+  return users.filter((user) => isUser(user, ['nickName']) && regexp.test(user.nickName));
 };
 
 export default userByNickName;
