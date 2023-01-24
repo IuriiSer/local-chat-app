8f33a3950e2abd94a2447df8c7f4262d833a1b2e
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:51:02 +0300

    add logic to subscribe for new messages

---
+++ src/lib/subscribers/subscribeForNewMessages.ts
@@ -0,0 +1,23 @@
+import { ChatID } from '../../DataTypes/Chat/Chat.D';
+import {
+  BroadcastAnyMessage,
+  isNewMessage,
+  NewMessage,
+} from '../interfaces/broadcastChannel/BroadcastInterface.D';
+
+const subscribeForNewMessages =
+  (chatID: ChatID | ChatID[], updateMessages: (data: NewMessage) => void) =>
+  (data: BroadcastAnyMessage) => {
+    if (!isNewMessage(data)) return;
+    if (Array.isArray(chatID) && !chatID.some((subChatID) => subChatID === data.content.chatID))
+      return;
+    if (typeof chatID === 'string' && data.content.chatID !== chatID) return;
+    updateMessages(data);
+
+    console.group('EVENT NewMessages');
+    console.log('Sunscries for Chats', chatID);
+    console.log('BroadcastAnyMessage', data);
+    console.groupEnd();
+  };
+
+export default subscribeForNewMessages;
