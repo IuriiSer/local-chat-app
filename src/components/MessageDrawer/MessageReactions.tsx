5ddd75bc0185c6efb8109575fbfa8e2fc27d9d67
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:41:04 +0300

    Message reactions drawer

---
+++ src/components/MessageDrawer/MessageReactions.tsx
@@ -0,0 +1,11 @@
+import { MessageReaction } from '../../DataTypes/Message/MessageReaction.D';
+
+interface MessageReactionsI {
+  reactions: MessageReaction[];
+}
+
+const MessageReactions = ({ reactions }: MessageReactionsI) => {
+  return <></>;
+};
+
+export default MessageReactions;
