2cf5b3cfbaf0a7ebb7aea47a74418709748c8ef3
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:37:09 +0300

    add new services

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import AppContext from './components/AppContext/AppContext';
 import Authorization from './components/Authorization/Authorization';
 import Messenger from './components/Messenger/Messenger';
+import useBroadcastService from './hooks/useBroadcastService/useBroadcastService';
 import useChatService from './hooks/useChatService/useChatService';
+import useMessageService from './hooks/useMessageService/useMessageService';
 import useUserService from './hooks/useUserService/useUserService';
 
 function App() {
   const userService = useUserService();
-  const chatService = useChatService(userService.authorizedUser);
+  const bcService = useBroadcastService(userService.authorizedUser);
+  const chatService = useChatService(
+    userService.authorizedUser,
+    userService.updateUserState,
+    bcService.sentData,
+  );
+  const messageService = useMessageService(userService.authorizedUser, bcService.sentData);
 
   return (
-    <AppContext.Provider value={{ userService, chatService }}>
+    <AppContext.Provider value={{ userService, chatService, bcService, messageService }}>
       <Authorization />
       {userService.authorizedUser && <Messenger />}
     </AppContext.Provider>
