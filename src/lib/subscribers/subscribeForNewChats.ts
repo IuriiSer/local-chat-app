cea72b965ac93514127b4aae419058ff395848f6
IuriiSer <yusergeevi4@gmail.com>
2023-01-24 21:50:44 +0300

    add log for event

--- src/lib/subscribers/subscribeForNewChats.ts
+++ src/lib/subscribers/subscribeForNewChats.ts
@@ -9,6 +9,11 @@ const subscribeForNewChats =
     if (!isInvitedToChat(data)) return;
     if (data.content.to !== userID) return;
     updateChats();
+
+    console.group('EVENT NewChats');
+    console.log('ReciverUser', userID);
+    console.log('BroadcastAnyMessage', data);
+    console.groupEnd();
   };
 
 export default subscribeForNewChats;
