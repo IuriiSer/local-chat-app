38a100c5f185dac700a19d0e8d9eadb91ece30da
IuriiSer <yusergeevi4@gmail.com>
2023-01-25 10:22:19 +0300

    update type

--- src/hooks/useMessageService/useMessageService.D.ts
+++ src/hooks/useMessageService/useMessageService.D.ts
@@ -1,8 +1,13 @@
-import { ChatID } from "../../DataTypes/Chat/Chat.D";
-import { Message, MessageID } from "../../DataTypes/Message/Message.D";
+import { ChatID } from '../../DataTypes/Chat/Chat.D';
+import { Message, MessageID, MessageMetafields } from '../../DataTypes/Message/Message.D';
+import { MessageNewReaction } from '../../DataTypes/Message/MessageReaction.D';
 
 export type UseMessageService = {
   getMessages: (chatID: ChatID) => Message[];
   getMessage: (messageID: MessageID) => Message | null;
-  addMessage: (chatID: string, messageText: string) => Message | null;
+  sentMessage: (chatID: string, messageText: string, extraData?: MessageMetafields) => Message | null;
+  sentReaction: (chatID: string, message: Message, reaction: MessageNewReaction) => void;
+    // you should pass updated message data
 };
+
+// info about messages type are in interface
