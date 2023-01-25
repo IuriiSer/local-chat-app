512207fc11dbacd4cfd0dc188f3abd89a63594cd
IuriiSer <yusergeevi4@gmail.com>
2023-01-25 10:24:49 +0300

    prettier changes

--- src/lib/subscribers/index.ts
+++ src/lib/subscribers/index.ts
@@ -1,6 +1,11 @@
+import subscribeForMessageGetReaction from './subscribeForMessageGetReaction';
 import subscribeForNewChats from './subscribeForNewChats';
 import subscribeForNewMessages from './subscribeForNewMessages';
 
-const subscribers = { forNewChats: subscribeForNewChats, forNewMessages: subscribeForNewMessages };
+const subscribers = {
+  forNewChats: subscribeForNewChats,
+  forNewMessages: subscribeForNewMessages,
+  forMessageGetReaction: subscribeForMessageGetReaction,
+};
 
 export default subscribers;
